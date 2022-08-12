b85723658b94485b055918177b06f31111948b45
Romulo Ramos <ti.romuloramos@gmail.com>
2022-08-11 21:34:57 -0300

    Primeiras aulas do projeto

---
+++ projeto_vue/src/components/LifeCycle.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1>Meu Sobrenome é: {{surname}}</h1>
+</template>
+
+<script>
+    export default {
+        name: 'LifeCycle',
+        data () {
+            return {
+                surname: 'Ainda não sei',
+            }
+        },
+        created() {
+            setTimeout(() => {
+                this.surname = 'Ramos'
+            }, 1000);
+            
+        }
+    }
+</script>
---
+++ projeto_vue/src/components/Pessoa.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h2>Esta é a descrição da Pessoa: {{nome}}</h2>
+        <Info/>
+        
+    </div>
+</template>
+
+<script>
+
+    import Info from './Info';
+
+    export default {
+        name: "Pessoa",
+        data() {
+            return {
+                nome: "Romulo",
+            };
+        },
+        components: {
+            Info
+        }
+}   
+</script>
